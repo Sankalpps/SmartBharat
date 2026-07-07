d4f104f105931cac6f645b09ac57767ead9abb57
Sankalp P S <sankalpps2020@gmail.com>
2026-07-07 14:09:36 +0530

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SmartBharat from './SmartBharat.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SmartBharat />
+  </React.StrictMode>,
+)
